7186d3d26dd7d8b8a66d93302f8310d5b1c1cc9c
Salvador Sosa <salvasoss@icloud.com>
2023-09-21 21:20:49 -0300

    primera actividad javascript

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+let nombre = prompt ("Ingrese  Su Nombre");
+
+alert ("Hola" + " " + nombre + " " + "Buenas noches!");
+
+let edad = Number(prompt( "Cuantos años tienes?"))
+
+alert (edad * 365 + " " + "es tu edad en dias!");
+
+let edadEnDias = edad * 365;
+
+ let mesDeNacimiento = prompt ("En que mes naciste?")
+
+ let ocupacion = prompt ("Cual es tu ocupacion actual?")
+
+ alert ("Tu nombre es" + " " + nombre + ", tienes" + " " + edadEnDias + " " + "dias o" + " " + edad + " " + "años." + " " + "Naciste en el mes de" + " " + mesDeNacimiento + " " + "y actualmente tu ocupacion actual es:" + " " + ocupacion)
+
+alert ("Gracias" + " " + nombre + " " + "por tu cooperacion!")
