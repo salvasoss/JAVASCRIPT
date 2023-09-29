5a29cd189c586080e820bebfb82eb4f6de892cad
Salvador Sosa <salvasoss@icloud.com>
2023-09-28 21:32:07 -0300

    clase 3, seguir practicandoooo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// CLASE 1
 
 // let nombre = prompt ("Ingrese  Su Nombre");
 
@@ -16,24 +17,34 @@
 //  alert ("Tu nombre es" + " " + nombre + ", tienes" + " " + edadEnDias + " " + "dias o" + " " + edad + " " + "años." + " " + "Naciste en el mes de" + " " + mesDeNacimiento + " " + "y actualmente tu ocupacion actual es:" + " " + ocupacion)
 
 // alert ("Gracias" + " " + nombre + " " + "por tu cooperacion!")
-alert ( "Licencia de Conducir");
-let nombre = prompt ( "Ingrese su nombre").toLowerCase();
 
-let edad = Number (prompt ("Ingrese su edad"));
+// CLASE 2
 
-if ((nombre !="") && (edad !="")) {
-    alert("Nombre: "+nombre +"Apellido: "+edad);
-} else{
-    alert ("Error: campos requeridos")
-}
+// alert ( "Licencia de Conducir");
+// let nombre = prompt ( "Ingrese su nombre").toLowerCase();
 
-if (edad <17) {
-    alert (`Lo siento ${nombre}, pero debes esperar a los 17 o mas para manejar`);
-}
-else if (edad == 17) {
-    alert (`${nombre}, puedes sacar el carnet, pero solo si tus padres aceptan emanciparte `);
-}
-else {
-    alert (`${nombre}, puedes sacar tu carnet de conducir libremente`);
-}
+// let edad = Number (prompt ("Ingrese su edad"));
 
+// if ((nombre !="") && (edad !="")) {
+//     alert("Nombre: "+nombre +"Apellido: "+edad);
+// } else{
+//     alert ("Error: campos requeridos")
+// }
+
+// if (edad <17) {
+//     alert (`Lo siento ${nombre}, pero debes esperar a los 17 o mas para manejar`);
+// }
+// else if (edad == 17) {
+//     alert (`${nombre}, puedes sacar el carnet, pero solo si tus padres aceptan emanciparte `);
+// }
+// else {
+//     alert (`${nombre}, puedes sacar tu carnet de conducir libremente`);
+// }
+
+// CLASE 3 
+// tabla de multiplicar
+let numero = Number (prompt ("Ingrese un numero"))
+for (let i = 1 ; i <=10; i++){
+    let resultado = numero * i;
+    alert (`${resultado}`)
+}
